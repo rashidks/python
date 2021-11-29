8df45658f608fad19590f08c86e710f45b69ab90
rashidks <muhammedrashiks1@gmail.com>
2021-11-29 09:27:36 +0530

    second commit

---
+++ stringwork.py
@@ -0,0 +1,7 @@
+s='learning python is fun !'
+print("length of ",s,"is",len(s))
+print(" string in lower case is",s.lower())
+print("string in upper case is",s.upper())
+print("changing of case of string is ",s.swapcase())
+print(" 1 st letter of string in upper case is",s.capitalize())
+print("string with first letter of all word is capitalized is ",s.title())
